-corrected objective computation for weighted ssvm, minor threshold changes

pystruct/learners/n_slack_ssvm_weights.py
@@ -175,7 +175,7 @@ class NSlackSSVMWeighted(BaseSSVM):
         h = cvxopt.matrix(np.hstack((tmp1, tmp2, zero_constr)))
 
         # solve QP model
-        cvxopt.solvers.options['feastol'] = 1e-5
+        cvxopt.solvers.options['feastol'] = 1e-6
         try:
             solution = cvxopt.solvers.qp(P, q, G, h)
         except ValueError:
@@ -200,13 +200,13 @@ class NSlackSSVMWeighted(BaseSSVM):
             print("%d support vectors out of %d points" % (np.sum(sv),
                                                            n_constraints))
             # calculate per example box constraint:
-            print("Box constraints at C: %d" % np.sum(1 - box / C < 1e-3))
+            print("Box constraints at C: %d" % np.sum((1 - box / (C * weights)) < 1e-3))
             print("dual objective: %f" % -solution['primal objective'])
         self.w = np.dot(a, joint_feature_matrix)
         return -solution['primal objective']
 
-    def _check_bad_constraint(self, y_hat, slack, old_constraints):
-        if slack < 1e-5:
+    def _check_bad_constraint(self, y_hat, slack, old_constraints, weight):
+        if weight*slack < 1e-6:
             return True
         y_hat_plain = unwrap_pairwise(y_hat)
 
@@ -229,9 +229,9 @@ class NSlackSSVMWeighted(BaseSSVM):
                 # if slack of new constraint is smaller or not
                 # significantly larger, don't add constraint.
                 # if smaller, complain about approximate inference.
-                if slack - slack_tmp < -1e-5:
+                if weight*(slack - slack_tmp) < -1e-6:
                     if self.verbose > 0:
-                        print("bad inference: %f" % (slack_tmp - slack))
+                        print("bad inference: %f" % (weight*(slack_tmp - slack)))
                     if self.break_on_bad:
                         raise ValueError("bad inference: %f" % (slack_tmp -
                                                                 slack))
@@ -335,7 +335,7 @@ class NSlackSSVMWeighted(BaseSSVM):
                             continue
 
                         if self._check_bad_constraint(y_hat, slack,
-                                                      constraints[i]):
+                                                      constraints[i], weights[i]):
                             continue
 
                         constraints[i].append([y_hat, delta_joint_feature, loss])
@@ -397,7 +397,7 @@ class NSlackSSVMWeighted(BaseSSVM):
         if verbose:
             print("Computing final objective.")
         self.timestamps_.append(time() - self.timestamps_[0])
-        self.primal_objective_curve_.append(self._objective(X, Y))
+        self.primal_objective_curve_.append(self._objective(X, Y, weights))
         self.objective_curve_.append(objective)
         if self.logger is not None:
             self.logger(self, 'final')
@@ -432,3 +432,4 @@ class NSlackSSVMWeighted(BaseSSVM):
             for j in np.where(to_remove)[0][::-1]:
                 del sample[j]
             assert(len(sample) == len(self.last_active[i]))
+

pystruct/tests/test_inference/test_mixed_ogm.py
@@ -8,12 +8,12 @@ __author__ = 'tolgab'
 def test_tree():
     # build tree
     n_nodes = 6
-    n_states_per_node = [3, 3, 2, 2, 2, 2]
+    n_states_per_node = [3, 3, 1, 2, 2, 2]
     # make edges
     edges = np.array([[0, 2], [1, 2], [2, 3], [2, 4], [3, 5]], dtype=int)
     n_edges = len(edges)
     # these have high unary pots, the others have 0 unary pots
-    correct_states = [0, 0, 1, 1, 0, 1]
+    correct_states = [2, 0, 0, 1, 0, 1]
     # fill unary pots
     unary_pots = np.zeros(n_nodes, dtype=object)
     for i in range(n_nodes):
@@ -26,7 +26,9 @@ def test_tree():
                                n_states_per_node[edges[i][1]]), dtype=float)
         pw_pots[i][correct_states[edges[i][0]], correct_states[edges[i][1]]] = 15.
     res = inference_dispatch(unary_pots, pw_pots, edges, "mixed_ogm")
-    assert_array_equal(res,correct_states)
+    print correct_states
+    print res
+    assert_array_equal(res, correct_states)
 
 if __name__ == "__main__":
     test_tree()

pystruct/utils/inference.py
@@ -109,7 +109,7 @@ def loss_augmented_inference(model, x, y, w, relaxed=True):
 
 
 # easy debugging
-def objective_primal(model, w, X, Y, C, variant='n_slack', n_jobs=1):
+def objective_primal(model, w, X, Y, C, weights, variant='n_slack', n_jobs=1):
     objective = 0
     constraints = Parallel(
         n_jobs=n_jobs)(delayed(find_constraint)(
@@ -119,6 +119,9 @@ def objective_primal(model, w, X, Y, C, variant='n_slack', n_jobs=1):
 
     if variant == 'n_slack':
         slacks = np.maximum(slacks, 0)
+    elif variant == 'n_slack_weighted':
+        slacks = np.maximum(slacks, 0)
+        slacks *= weights
 
     objective = max(np.sum(slacks), 0) * C + np.sum(w ** 2) / 2.
     return objective